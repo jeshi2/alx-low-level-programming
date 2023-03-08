d7bce563f189ae7cab4c62d08d194418369e5b09
jeshi2 <anonymousveans@gmail.com>
2023-03-08 08:54:47 -0800

    recursion

---
+++ 0x08-recursion/main.h
@@ -0,0 +1,28 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+int _putchar(char c);
+
+void _puts_recursion(char *s);
+
+void _print_rev_recursion(char *s);
+
+int _strlen_recursion(char *s);
+
+int factorial(int n);
+
+int _pow_recursion(int x, int y);
+
+int _sqrt_recursion(int n);
+
+int is_prime_number(int n);
+
+int is_palindrome(char *s);
+
+int palindrome_checker(char *s, int len, int letter1, int letter2);
+
+int is_prime_helper(int n, int i);
+
+int _sqrt_helper(int n, int i);
+
+#endif /* MAIN_H */
