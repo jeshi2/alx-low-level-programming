d929620da52eb7fe74596dafb8eb2ba5f2bd3ea2
jeshi2 <jamesvean19@gmail.com>
2023-04-10 11:43:50 +0300

    here we are people

--- 0x14-bit_manipulation/3-set_bit.c
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -7,7 +7,13 @@ int set_bit(unsigned long int *n, unsigned int index)
 		return (-1); /* if index is > 63 funct retun -1 failed
 	
 	// we set bit at index to 1 << val 1 by index bit without affecting sets */
-	*n =+ 1 << index;	
-	return (1);
+	*n =((1UL << index | *n));
+	{
+		*n =+ 1 << index;	
+		return (1);
+	}
+	return(-1);
+
+	
 }
 
---
+++ README.md
@@ -0,0 +1,2 @@
+welcome to c programming content written with Antony Evans
+in respect with Alx Software Engineering
